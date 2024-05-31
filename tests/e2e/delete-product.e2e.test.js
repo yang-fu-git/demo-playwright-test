5f94558bee832084bdab6df085bc35831b162a9c
yang-fu-git <yangf0129@outlook.com>
2024-05-31 12:49:39 +0200

    add e2e for delete func

---
+++ tests/e2e/delete-product.e2e.test.js
@@ -0,0 +1,41 @@
+const { test, expect } = require('@playwright/test')
+
+const { createUser, getProductBody } = require('../../lib/helpers')
+
+test.describe('Create product', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/cadastrarusuarios')
+    await createUser(page)
+
+    await page.waitForNavigation()
+    await expect(page).toHaveURL('/admin/home')
+  })
+
+  test('delete a product successfully', async ({ page }) => {
+    const product = getProductBody()
+    await page.click('data-testid=cadastrarProdutos') 
+
+    await page.type('data-testid=nome', product.nome)
+    await page.type('data-testid=preco', product.preco)
+    await page.type('data-testid=descricao', product.descricao)
+    await page.type('data-testid=quantity', product.quantidade)
+
+    await page.click('data-testid=cadastarProdutos')
+
+    await page.waitForNavigation()
+    await expect(page).toHaveURL('/admin/listarprodutos')
+
+    const cellLocator = page.locator('tr').filter({ hasText: product.nome })
+
+    for (const value of Object.values(product)) {
+      await expect(cellLocator.filter({ hasText: value })).toBeVisible()
+    }
+
+    // start to delete
+    const deleteButton = cellLocator.locator('button').filter({hasText:'Excluir'})
+    await deleteButton.click()
+    await page.waitForNavigation()
+    
+    expect(page.locator('tr').filter({ hasText: product.nome })).toHaveCount(0)
+  })
+})
